2b91adf19bbce245a37fedbf5c8c55a536d13c85
Alain Frisch <alain@frisch.fr>
2006-05-16 14:27:42 +0000

    New camlp4
    git-svn-id: http://sedlex.googlecode.com/svn/trunk@25 440dde74-9ba2-753d-f1af-a172a80003eb

--- mk_pa_ulex.ml
+++ mk_pa_ulex.ml
@@ -1,10 +1,14 @@
-let s = float_of_string (String.sub (Sys.ocaml_version) 0 4) in
+if Sys.command "camlp4of 2> /dev/null" = 0 then (
+  print_endline "New camlp4";
+  Sys.command "cp pa_ulex.ml.new pa_ulex.ml"
+) 
+else let s = float_of_string (String.sub (Sys.ocaml_version) 0 4) in
 if (s < 3.09) then (
   print_endline "Old camlp4 (loc)";
   Sys.command "sed s/_loc/loc/ < pa_ulex.ml.src > pa_ulex.ml"
 )
 else  (
-  print_endline "New camlp4 (_loc)";
+  print_endline "Old camlp4 (_loc)";
   Sys.command "cp pa_ulex.ml.src pa_ulex.ml"
 )
 
